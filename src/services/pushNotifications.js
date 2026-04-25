e6813eefb3c8cd5841960acd1e871fcd32d841c5
Adriel Azevedo <adrielsatos198@gmail.com>
2026-04-25 14:11:06 -0300

    Add push notifications and fix login

--- src/services/pushNotifications.js
+++ src/services/pushNotifications.js
@@ -1,17 +1,14 @@
 import { getToken } from "firebase/messaging";
 import { messaging } from "./firebase";
 
-export const requestPushPermission = async (userId) => {
+export const requestPushPermission = async () => {
   const permission = await Notification.requestPermission();
+
   if (permission !== "granted") return;
 
   const token = await getToken(messaging, {
-    vapidKey: "BPZCclanQpjkzsRgC6RJdMCcKox0xmeNlMPGtjHAQk8SY9t7Em4mPtFgpEY-qrdmjoahRaz-Q3tAZGNHLkJ4nq8",
+    vapidKey: "SUA_VAPID_KEY",
   });
 
-  await fetch("/api/save-token", {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({ userId, token }),
-  });
+  console.log("TOKEN:", token);
 };
